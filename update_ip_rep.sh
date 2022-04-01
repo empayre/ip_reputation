b840e42c4db78c9f786abde4f1bd52df940ae57a
joker2013 <ivan.joker2009@mail.ru>
2022-04-01 09:23:30 +0300

    Create update_ip_rep.sh

---
+++ update_ip_rep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -Rf /etc/suricata/iprep/*
+python3 /opt/OTX-Suricata-master/otx-suricata/suricata.py --key 3f59b21830614df4e88ef727cc576a89a5bd58d831a01856ff34a9f65035b51c --skip-filemd5  --destination-directory /etc/suricata/iprep/
+curl -Lo /tmp/talos_ip "http://www.talosintelligence.com/documents/ip-blacklist"
+sed "s/$/,41,127/" /tmp/talos_ip >> /etc/suricata/iprep/reputation.list
+systemctl restart suricata
